4a1c6efdf8c025d9d80bdab85008ff3a8bd61a9c
Justin Ramirez <jusrez528@gmail.com>
2022-04-13 22:13:58 -0400

    Add booleans

--- variables.rb
+++ variables.rb
@@ -8,5 +8,9 @@ numero_tres = 3
 numero_quatro = 4
 #variable numero_cinco is declared and assigned to the Float of 5.55
 numero_cinco = 5.55
-#variable numero_seis is declaed and assigned to the Float of 6.67
+#variable numero_seis is declared and assigned to the Float of 6.67
 numero_seis = 6.67
+#variable numero_siete is declared and assigned to the Boolean of True
+numero_siete = True
+#variable numero_ocho is declared and assigned to the Boolean of False
+numero_ocho = False
