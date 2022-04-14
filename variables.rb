36960202e1b73e215eb2ca165ea503b08a9ad5ae
Justin Ramirez <jusrez528@gmail.com>
2022-04-13 22:09:54 -0400

    Add integers

--- variables.rb
+++ variables.rb
@@ -2,3 +2,7 @@
 numero_uno = "Hello"
 #variable numero_dos is declared and assigned to the String of "There"
 numero_dos = "There"
+#variable numero_tres is declared and assigned to the Integer of 3
+numero_tres = 3
+#variable numero_quatro is declared and assigned to the Integer of 4
+numero_quatro = 4
