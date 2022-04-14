936291f1f4961d1b118d23f523bac1e519f7abfb
Justin Ramirez <jusrez528@gmail.com>
2022-04-13 22:06:57 -0400

    Add strings

---
+++ variables.rb
@@ -0,0 +1,4 @@
+#variable numero_uno is declared and assigned to the String of "Hello"
+numero_uno = "Hello"
+#variable numero_dos is declared and assigned to the String of "There"
+numero_dos = "There"
